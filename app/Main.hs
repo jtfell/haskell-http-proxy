ed612fbf2f3d865a3700709e35185040f62c204b
Julian Fell <jtfell@users.noreply.github.com>
2017-07-13 16:14:46 +1000

    clean up main server interactions

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,10 @@ import Network (listenOn, accept, PortID(..), Socket)
 import Network.Socket hiding (recv, accept)
 import Network.Socket.ByteString (recv, sendAll)
 import System.Environment (getArgs)
-import System.IO (hPrint, hGetLine, hPutStrLn, Handle)
+import System.IO (hPrint, hGetLine, hPutStrLn, Handle, hSetBuffering,
+                  BufferMode(NoBuffering))
 import Control.Concurrent (forkIO)
+import Data.Monoid
 
 import qualified Data.ByteString as BS
 
@@ -22,52 +24,58 @@ main = do
     print $ "Listening on " ++ head args
     sockHandler sock
 
--- Handle requests from the socket concurrently - uses lightweight threads
+-- Handle requests from the socket concurrently - recursively spawns
+-- lightweight green threads.
 sockHandler :: Socket -> IO ()
 sockHandler sock = do
     (hdl, _, _) <- accept sock
-    forkIO $ proxyRequest hdl
+    forkIO $ handler hdl
     sockHandler sock
 
--- Creates a request and sends it to the "ProxyPass" location
-proxyRequest :: Handle -> IO ()
-proxyRequest hdl = do
+-- Reads a request, sends it to the "ProxyPass" location and relays the
+-- response back to the original client.
+handler :: Handle -> IO ()
+handler hdl = do
+     hSetBuffering hdl NoBuffering
+     req <- readRequest hdl 
+     res <- proxyRequest req
+     sendRequest hdl res
 
-    -- Efficient way to get bytestrings from the network (I think?)
-    str <- BS.hGetContents hdl
+--
+-- External interactions for each request
+--
+readRequest :: Handle -> IO HttpRequest
+readRequest hdl = (fromRight . parseRequest) <$> BS.hGetContents hdl
 
-    print str
+sendRequest :: Handle -> HttpRequest -> IO ()
+sendRequest hdl req = hPrint hdl $ printRequest req
 
-    -- This is a pure function, so we can't bind it in the monadic context
-    let req = fromRight $ parseRequest str
-
-    -- Parse the bytestring and print the HTTP request object
-    print $ printRequest req
-
-    res <- sendRequest req
-
-    print $ show res
-
-    -- Proxy the response back to the client
-    -- hPrint hdl $ show $ responseBody response
-
-fromRight           :: (Show a) => Either a b -> b
-fromRight (Left x)  = error ("fromRight: Argument takes form 'Left " ++ show x ++ "'")
-fromRight (Right x) = x
-
-sendRequest :: HttpRequest -> IO BS.ByteString
-sendRequest req = do
+proxyRequest :: HttpRequest -> IO HttpRequest
+proxyRequest req = do
 
     -- Interrogate DNS for localhost:3000
     addrinfos <- getAddrInfo Nothing (Just "") (Just "3000")
     let serveraddr = head addrinfos
 
-    -- create the TCP socket
+    -- Create the TCP socket
     sock <- socket (addrFamily serveraddr) Stream defaultProtocol
     connect sock (addrAddress serveraddr)
 
+    -- Send the request over the TCP socket
+    print $ "Sending to backend:   " <> printRequest req 
     sendAll sock $ printRequest req
+
+    -- Get the response back from the backend server
     msg <- recv sock 1024
+    print $ "Received from backend:   " <> msg
+
+    -- Close the socket
     close sock
 
-    return msg
+    -- Return the parsed response for sending back to the original client
+    return $ fromRight $ parseRequest msg
+
+fromRight           :: (Show a) => Either a b -> b
+fromRight (Left x)  = error ("fromRight: Argument takes form 'Left " ++ show x ++ "'")
+fromRight (Right x) = x
+
